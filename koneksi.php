e3b098d9b983dde999e03079a6f42c2ccede2bdb
raditfirmanzah <116476187+raditfirmanzah@users.noreply.github.com>
2023-01-11 22:09:04 +0700

    update koneksi
    koenski

--- koneksi.php
+++ koneksi.php
@@ -7,6 +7,8 @@ $nama_database = "dealer";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
-if (!$db) {
+if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
+
+?>
