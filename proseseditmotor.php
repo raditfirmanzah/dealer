5b7aff5f5ade0e849ca6333d252c578ff77ecc12
raditfirmanzah <116476187+raditfirmanzah@users.noreply.github.com>
2023-01-12 14:11:50 +0700

    proses edit motor

--- proseseditmotor.php
+++ proseseditmotor.php
@@ -8,18 +8,21 @@ if(isset($_POST['simpan'])){
    
     $id = $_POST['id'];
     $id_user = $_POST['id_user'];
-    $id_merk = $_POST['username'];
-    $no_hp = $_POST['no_hp'];
-    $password = $_POST['password'];
+    $id_merk = $_POST['id_merk'];
+    $warna = $_POST['warna'];
+    $kode_mesin = $_POST['kode_mesin'];
+    $cc = $_POST['cc'];
+    $no_rangka = $_POST['no_rangka'];
+    $no_polisi = $_POST['no_polisi'];
+    $total = $_POST['total'];
 
-
-    $sql = "UPDATE user SET nama='$nama',  username='$username', no_hp='$no_hp', password='$password' WHERE id=$id";
+    $sql = "UPDATE motor SET id_user='$id_user', id_merk='$id_merk', warna='$warna', kode_mesin='$kode_mesin', cc='$cc', no_rangka='$no_rangka', no_polisi='$no_polisi', total='$total' WHERE id=$id";
     $query = mysqli_query($db, $sql);
 
     
     if( $query ) {
        
-        header('Location: list-reservasi.php');
+        header('Location: listmotor.php');
     } else {
        
         die("Gagal menyimpan perubahan...");
