dddea3f42c524f809e702d0b585e02e410a129b5
raditfirmanzah <116476187+raditfirmanzah@users.noreply.github.com>
2023-01-13 12:57:30 +0700

    update crud + yamaha.png

--- crud.php
+++ crud.php
@@ -3,59 +3,55 @@ include('head.php');
 
 ?>
 <div class="home">
-  <div class="container">
+    <div class="container">
 
-    <div class="row">
-      <!-- gambar -->
-      <div class="col-8">
-        <div id="carouselExampleDark" class="carousel carousel-dark slide">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="10000">
-              <img src="logo.jpg" class="d-block" alt="..." style="width: 900px; height: 650px;">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
-              </div>
+        <div class="row">
+            <!-- gambar -->
+            <div class="col-8">
+                <div id="carouselExampleDark" class="carousel carousel-dark slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+                            aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="10000">
+                            <img src="logo.jpg" class="d-block" alt="..." style="width: 900px; height: 650px;">
+                           
+                        </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                            <img src="yamaha.png" class="d-block" alt="..." style="width: 900px; height: 650px;">
+                            
+                        </div>
+                        <div class="carousel-item">
+                            <img src="gb.png" class="d-block" alt="..." style="width: 900px; height: 650px;">
+                            
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
-            <div class="carousel-item" data-bs-interval="2000">
-              <img src="motor.jpg" class="d-block" alt="..." style="width: 900px; height: 650px;">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img src="..." class="d-block" alt="...">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-              </div>
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
+
+
+            <!-- akhir gambar -->
+            <div class="col-4"></div>
         </div>
-      </div>
 
 
-      <!-- akhir gambar -->
-      <div class="col-4"></div>
+
     </div>
-
-
-
-  </div>
 </div>
 <?php
 include('bottom.php');
@@ -66,7 +62,7 @@ include('bottom.php');
 
 
 <?php if (isset($_GET['status'])) : ?>
-  <p class="status" style="text-align: center; font-size: xx-large;">
+<p class="status" style="text-align: center; font-size: xx-large;">
     <?php
     if ($_GET['status'] == 'sukses') {
       echo "Pendaftaran berhasil!";
@@ -74,5 +70,5 @@ include('bottom.php');
       echo "Pendaftaran gagal!";
     }
     ?>
-  </p>
+</p>
 <?php endif; ?>
