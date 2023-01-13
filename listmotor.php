0a87964ad6c8d36447aabd6c4fcb920051fd75f1
Raya Ghaniyya <rayaghaniyyayuda@gmail.com>
2023-01-13 14:20:01 +0700

    update tabel

--- listmotor.php
+++ listmotor.php
@@ -23,7 +23,7 @@ include("head.php")
     <div class="table-responsive">
         <table class="table table-hover">
             <thead>
-                <tr  class="table-secondary">
+                <tr class="table-secondary">
                     <th>Id</th>
                     <th>Id User</th>
                     <th>Id Merk</th>
@@ -76,10 +76,10 @@ include("head.php")
 
 <div class="container text-center">
     <div class="row justify-content-center">
-        <div class="col-4 pt-4 pe-5">
+        <div class="col-2 pt-4 pe-5">
             <p>Total: <?php echo mysqli_num_rows($query) ?></p>
         </div>
-        <div class="col-4">
+        <div class="col-3">
             <nav>
                 <a href="form-motor.php" class="btn btn-light" style="margin-bottom: 1rem; margin-top:1rem; margin-left:2rem;">[+] Tambah Baru</a>
             </nav>
--- listuser.php
+++ listuser.php
@@ -54,12 +54,12 @@ include('head.php');
 
 <div class="container text-center">
     <div class="row justify-content-center">
-        <div class="col-4 pt-4 pe-5">
+        <div class="col-2 pt-4 pe-5">
             <p>
                 Total: <?php echo mysqli_num_rows($query) ?>
             </p>
         </div>
-        <div class="col-4">
+        <div class="col-3">
             <nav>
                 <a href="form-user.php" class="btn btn-light" style="margin-bottom: 1rem; margin-top:1rem; margin-left:2rem;">[+] Tambah Baru</a>
             </nav>
