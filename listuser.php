34e14fa5aa73776145d6ffd449845f9182d5a990
luqmanhakim123 <114430708+luqmanhakim123@users.noreply.github.com>
2023-01-13 15:07:12 +0700

    Update listuser.php

--- listuser.php
+++ listuser.php
@@ -38,7 +38,7 @@ include('head.php');
 
                     echo "<td>";
                     echo "<a href=edituser.php?id=$user[id] class='btn btn-warning'>Edit</a> | ";
-                    echo '<a href="hapususer.php?id= ' . $user['id'] . '" class="btn btn-danger">Hapus</a>';
+                    echo '<a  href="hapususer.php?id= ' . $user['id'] . '" class="btn btn-danger">Hapus</a>';
                     echo "</td>";
 
                     echo "</tr>";
@@ -55,12 +55,12 @@ include('head.php');
 
 <div class="container text-center">
     <div class="row justify-content-center">
-        <div class="col-2 pt-4 pe-5">
+        <div class="col-4 pt-4 pe-5">
             <p>
                 Total: <?php echo mysqli_num_rows($query) ?>
             </p>
         </div>
-        <div class="col-3">
+        <div class="col-4">
             <nav>
                 <a href="form-user.php" class="btn btn-light" style="margin-bottom: 1rem; margin-top:1rem; margin-left:2rem;">[+] Tambah Baru</a>
             </nav>
